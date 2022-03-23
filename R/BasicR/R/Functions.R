ad005cb4df52d24801f4c4e8a2985a9b055053fa
Félix <felix.boudry@laposte.net>
2022-03-23 18:42:35 +0100

    Added functions to import multiple xlsx.

--- R/BasicR/R/Functions.R
+++ R/BasicR/R/Functions.R
@@ -52,6 +52,44 @@ import.csv.sep <-
     return(dataTables)
   }
 
+import.xlsx.sep <-
+  function(path_to_data,
+           file_type = "*.xlsx",
+           rows_to_import = "all",
+           columns_to_import = "all") {
+    csvFileList <-
+      list.files(path = path_to_data,
+                 pattern = file_type,
+                 full.names = TRUE)
+    dataTables <- lapply(csvFileList, read_excel, na.strings = c("", " ", "NA", "Na", "na"))
+    if (rows_to_import == "all" && columns_to_import == "all") {
+      for (i in 1:length(csvFileList)) {
+        dataTables[[i]] <- dataTables[[i]][]
+      }
+    }
+    else if (rows_to_import == "all" &&
+             columns_to_import != "all") {
+      for (i in 1:length(csvFileList)) {
+        dataTables[[i]] <- dataTables[[i]][, columns_to_import]
+      }
+    }
+    else if (rows_to_import != "all" &&
+             columns_to_import == "all") {
+      for (i in 1:length(csvFileList)) {
+        dataTables[[i]] <- dataTables[[i]][rows_to_import,]
+      }
+    }
+    else {
+      for (i in 1:length(csvFileList)) {
+        dataTables[[i]] <-
+          dataTables[[i]][rows_to_import, columns_to_import]
+      }
+    }
+    names(dataTables) <-
+      gsub("^.*/", "", csvFileList) #Delete path part and names data frames
+    return(dataTables)
+  }
+
 mergeAll <- function(DF1, DF2) {
   merge(DF1, DF2, all = TRUE)
 }
@@ -512,7 +550,7 @@ get.TTM.rel <- function(data_frame_list, cols_to_compute) {
         ), 1)))
     }
     colVals <- cbind(colVals, vals, row.names = NULL)
-    
+
     TTMDF <- cbind(TTMDF, colVals, row.names = NULL)
   }
   colnames(TTMDF) <- as.list(cols_to_compute)
